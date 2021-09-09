49a696d87288cd7cef6553ca37747ad88d65b8c2
mahdi sanatkar <sanatkar.dev@gmail.com>
2021-09-09 17:23:53 +0430

    remove counter

--- src/app/core/store.ts
+++ src/app/core/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../modules/counter/counterSlice';
 import authReducer from 'store/authSlice';
 import themeReducer from 'store/themeSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
     theme: themeReducer
   },
